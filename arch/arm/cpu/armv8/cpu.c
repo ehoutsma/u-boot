25c86bebbf3e07bc965294f26b819aa63d7998a5
Andre Przywara <andre.przywara@arm.com>
2016-05-28 16:15:05 +0100

    armv8: add simple sdelay implementation
    The sunxi DRAM setup code needs an sdelay() implementation, which
    wasn't defined for armv8 so far.
    Shamelessly copy the armv7 version and adjust it to work in AArch64.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- arch/arm/cpu/armv8/cpu.c
+++ arch/arm/cpu/armv8/cpu.c
@@ -16,6 +16,19 @@
 #include <asm/system.h>
 #include <linux/compiler.h>
 
+/************************************************************
+ * sdelay() - simple spin loop.  Will be constant time as
+ *  its generally used in bypass conditions only.  This
+ *  is necessary until timers are accessible.
+ *
+ *  not inline to increase chances its in cache when called
+ *************************************************************/
+void sdelay(unsigned long loops)
+{
+	__asm__ volatile ("1:\n" "subs %0, %1, #1\n"
+			  "b.ne 1b":"=r" (loops):"0"(loops));
+}
+
 int cleanup_before_linux(void)
 {
 	/*
